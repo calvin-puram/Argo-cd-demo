7f03ae5bfcbf0bc172b78593549b3883a62e0b49
calvin <puram.calvin@gmail.com>
2021-12-27 00:27:53 +0100

    set up version 1.1.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello There Argo CD Build 1.0.0</h1>");
+  res.send("<h1>Hello There Argo CD Build 1.1.1</h1>");
 });
 
 const port = process.env.PORT || 4000;
